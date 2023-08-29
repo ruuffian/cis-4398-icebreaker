c4843122cb1d0949bd9be7dc534adf20b1c36f4e
ruuffian <ruuffia@gmail.com>
2023-08-29 17:33:05 -0400

    Changed invocation to begin with a main() function call

--- main.py
+++ main.py
@@ -14,58 +14,63 @@ def writeToCalendarFile(filename, calendar):
 
 def usage():
     print("calexport file_name [-t <output_directory>]"
-headers = {
-    'Authorization': "Bearer {N9bjGKfycIBolbp0TbBL3cdaSWySJEbuuiwSZwfY}",
-    'Content-Type': "application/json",
-    'Accept': "application/json"
-}
 
-base_url = "https://courses.ianapplebaum.com"
-api = "/syllabus/4"
+def main():
+    headers = {
+        'Authorization': "Bearer {N9bjGKfycIBolbp0TbBL3cdaSWySJEbuuiwSZwfY}",
+        'Content-Type': "application/json",
+        'Accept': "application/json"
+    }
 
-response = requests.get(base_url + api, headers=headers)
-data = response.json()
+    base_url = "https://courses.ianapplebaum.com"
+    api = "/syllabus/4"
 
-events_data = data.get('events', [])
+    response = requests.get(base_url + api, headers=headers)
+    data = response.json()
 
-# Create a new calendar
-cal = Calendar()
+    events_data = data.get('events', [])
 
-current_time = datetime.datetime.now()
-# print(current_time)
+    # Create a new calendar
+    cal = Calendar()
 
-# Loop through events and add them to the calendar
-for event_data in events_data:
-    # print(event_data, "\n")
-    event = Event()
-    event.name = event_data["event_name"]
-    event.begin = event_data['event_date']
-    event.description = event_data["event_description"]
-    cal.events.add(event)
+    current_time = datetime.datetime.now()
+    # print(current_time)
 
-# Export the calendar to a file
-# First time this is run, create a base to compare to in the future
-try:
-    if not os.path.exists('base.ics'):
-        writeToCalendarFile('base.ics', cal)
+    # Loop through events and add them to the calendar
+    for event_data in events_data:
+        # print(event_data, "\n")
+        event = Event()
+        event.name = event_data["event_name"]
+        event.begin = event_data['event_date']
+        event.description = event_data["event_description"]
+        cal.events.add(event)
 
-    # if the base file exists, create a new file and compare it to the base
-    else:
-        writeToCalendarFile('my.ics', cal)
-        isSame = filecmp.cmp('base.ics', 'my.ics', shallow=False)
-        # if the base is not the same as the new file:
-        # remove the old base
-        # rename the new base
-        # send an email notification
-        if not isSame:
-            os.remove('base.ics')
-            os.rename('my.ics', 'base.ics')
-            print("The calendar has changed!")
-            # Send an email notification
+    # Export the calendar to a file
+    # First time this is run, create a base to compare to in the future
+    try:
+        if not os.path.exists('base.ics'):
+            writeToCalendarFile('base.ics', cal)
 
+        # if the base file exists, create a new file and compare it to the base
         else:
-            print("Same calendar :)")
+            writeToCalendarFile('my.ics', cal)
+            isSame = filecmp.cmp('base.ics', 'my.ics', shallow=False)
+            # if the base is not the same as the new file:
+            # remove the old base
+            # rename the new base
+            # send an email notification
+            if not isSame:
+                os.remove('base.ics')
+                os.rename('my.ics', 'base.ics')
+                print("The calendar has changed!")
+                # Send an email notification
 
-except UnicodeEncodeError:
-    print("In error")
-    pass
+            else:
+                print("Same calendar :)")
+
+    except UnicodeEncodeError:
+        print("In error")
+        pass
+
+if __name__ == "__main__":
+    main()
